dc67a884957f0f585f436dee952f29b0b62d95ed
iyuuya <vim.ito@1pac.jp>
2016-11-03 17:07:00 +0900

    letter opener routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
   }
   resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
