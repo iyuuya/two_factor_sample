30924a3e4df3dc209c6b44bda836b6c502a4e794
iyuuya <vim.ito@1pac.jp>
2016-11-03 16:42:27 +0900

    install devise-two-factor

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,24 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  group_id               :integer          not null
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
+#  id                        :integer          not null, primary key
+#  email                     :string(255)      default(""), not null
+#  encrypted_password        :string(255)      default(""), not null
+#  reset_password_token      :string(255)
+#  reset_password_sent_at    :datetime
+#  remember_created_at       :datetime
+#  sign_in_count             :integer          default(0), not null
+#  current_sign_in_at        :datetime
+#  last_sign_in_at           :datetime
+#  current_sign_in_ip        :string(255)
+#  last_sign_in_ip           :string(255)
+#  group_id                  :integer          not null
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  encrypted_otp_secret      :string(255)
+#  encrypted_otp_secret_iv   :string(255)
+#  encrypted_otp_secret_salt :string(255)
+#  consumed_timestep         :integer
 #
 # Indexes
 #
@@ -25,8 +29,15 @@
 #
 
 class User < ApplicationRecord
+  devise :two_factor_authenticatable,
+         :otp_secret_encryption_key => Settings.devise.two_factor.encrypt_key
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def otp_required_for_login
+    group.two_factor_auth
+  end
 end
